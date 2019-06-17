46d72929287f11e58d3c5eb125b5f245d3ef47fd
Do Pham Bao Hoang <franlkinclinton@gmail.com>
2019-06-18 00:07:01 +0700

    Add files via upload

---
+++ PokemonChecker.js
@@ -0,0 +1,19 @@
+async function getPokemon(name) {
+    let response = await fetch(`https://pokeapi.co/api/v2/pokemon/${name}/`)
+    let pokemonData = await response.json();
+    let imageUrl = pokemonData.sprites.front_default;
+    let img = document.getElementById("pokemon_img")
+    img.src = imageUrl;
+    let pokename = document.getElementById("name");
+    pokename.textContent = pokemonData.name.toUpperCase();
+    }
+
+
+let btn = document.getElementById("btn_get_pokemon")
+btn.addEventListener("click", () => {
+    let input = document.getElementById("pokemon_name")
+    getPokemon(input.value)
+})
+
+
+
