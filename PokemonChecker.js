6abc00dcaef2b16ad6abac42db7559d9bafdd4ee
Do Pham Bao Hoang <franlkinclinton@gmail.com>
2019-06-27 20:54:12 +0700

    Update PokemonChecker.js

--- PokemonChecker.js
+++ PokemonChecker.js
@@ -1,4 +1,3 @@
-
 async function getPokemon(name) {
     let response = await fetch(`https://pokeapi.co/api/v2/pokemon/${name}/`)
     let pokemonData = await response.json();
@@ -8,8 +7,6 @@ async function getPokemon(name) {
     let pokename = document.getElementById("name");
     pokename.textContent = pokemonData.name.toUpperCase();
     }
-
-
 let btn = document.getElementById("btn_get_pokemon")
 btn.addEventListener("click", () => {
     let input = document.getElementById("pokemon_name")
